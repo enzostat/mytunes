35fba849da781a29b45078f8af23557fc47bc324
stanstaton <thatkidenzo@gmail.com>
2019-08-26 09:54:34 -0700

    initial commit

---
+++ index.js
@@ -0,0 +1,30 @@
+//require in modules
+const express = require('express');
+const layouts = require('express-ejs-layouts');
+require('dotenv').config();
+const flash = require('connect-flash');
+const session = require('express-session');
+const passport = require('./config/passportConfig');
+
+//instantiate the express app
+const app = express();
+
+//set up middleware or settings
+app.set('view engine', 'ejs');
+app.use(layouts);
+app.use('/', express.static('static'));
+app.use(express.urlencoded({ extended: false }));
+app.use(session({
+	secret: process.env.SESSION_SECRET,
+	resave: false,
+	saveUninitialized: true
+
+}));
+app.use(flash()); //after session
+app.use(passport.initialize());
+app.use(passport.session());
+
+//routes
+app.use('/auth', require('./controllers/auth'));
+app.use('/artists', require('./controllers/artists'))
+app.use('/songs', require('./controllers/songs'))
