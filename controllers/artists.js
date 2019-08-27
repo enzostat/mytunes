5ae84daf3c5eb4a9de3a373827d4daf3052c11e4
stanstaton <thatkidenzo@gmail.com>
2019-08-26 21:39:18 -0700

    implemented searching for new artists and showing results

--- controllers/artists.js
+++ controllers/artists.js
@@ -1,6 +1,7 @@
 const router = require('express').Router();
 const db = require('../models');
 require('dotenv').config();
+const axios = require('axios');
 
 
 
@@ -11,6 +12,21 @@ router.get('/', (req,res) => {
 router.get('/new', (req,res) => {
     var url = ""
     res.render('artists/new')
+})
+
+router.get('/new/results', (req,res) => {
+    var query = req.query;
+    var url = "https://ws.audioscrobbler.com/2.0/?method=artist.search&artist="+query.name+"&api_key="+process.env.api_key+"&format=json"
+   console.log(url)
+    axios.get(url)
+    .then(response => {
+        var results = response.data.results;
+        res.render('artists/results', {results})
+    })
+    .catch(err => {
+        console.log(err)
+        res.redirect('../../*')
+    })
 })
 
 router.get('/:id', (req,res) => {
