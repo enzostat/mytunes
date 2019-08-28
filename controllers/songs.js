95bcb3f706012ae673d7a8acbacf8410baca8bd6
stanstaton <thatkidenzo@gmail.com>
2019-08-28 11:35:13 -0700

    implemented saving of songs to favorites

--- controllers/songs.js
+++ controllers/songs.js
@@ -7,6 +7,28 @@ router.get('/', (req,res) => {
     res.render('songs/index')
 })
 
+router.post('/', (req,res) => {
+    let userId = req.user.id;
+    let artistId = req.body.artistId;
+
+    db.song.findOrCreate({
+        where: {name: req.body.name},
+        defaults: req.body
+    }).spread((song, created) => {
+        if (userId > 0) {
+            db.user.findByPk(userId)
+            .then(user => {
+                song.addUser(user)
+            })
+            .catch(err => {
+                console.log(err)
+                res.send('something went wrong')
+            })
+        }
+        res.redirect('/songs')
+    })
+})
+
 router.get('/new', (req,res) => {
     res.render('songs/new')
 })
